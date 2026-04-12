91ddeb0654813097fc24d3804a9e6f0cfbe025aa
weatherquant <jasonmwest@hotmail.com>
2026-04-13 09:12:23 +1000

    roll back amend

--- apps/marketing/app/layout.tsx
+++ apps/marketing/app/layout.tsx
@@ -1,5 +1,3 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "../i18n";
 import { config } from "@config";
 import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
@@ -7,23 +5,13 @@ import type { PropsWithChildren } from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: {
-    absolute: config.appName,
-    default: config.appName,
-    template: `%s | ${config.appName}`,
-  },
+	title: {
+		absolute: config.appName,
+		default: config.appName,
+		template: `%s | ${config.appName}`,
+	},
 };
 
-export default async function RootLayout({ children, params }: PropsWithChildren & { params: { locale: string } }) {
-  const messages = await getMessages(params.locale);
-
-  return (
-    <html lang={params.locale}>
-      <body>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: PropsWithChildren) {
+	return children;
 }
